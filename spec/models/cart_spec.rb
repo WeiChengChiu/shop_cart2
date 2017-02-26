6de19b53b855907e4d577ad2b49a373547929662
Chiu Eric <c3366301@gmail.com>
2017-02-26 11:37:00 +0800

    refactor rspec instance

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
   describe "購物車基本功能" do
+    let(:cart) { Cart.new }
+    let(:p1) { Product.create(title: "海賊王", price:80) }
+    let(:p2) { Product.create(title: "美食的俘虜", price:100) }
+
     it "可以加商品到購物車，然後購物車有商品" do
-      cart = Cart.new
       cart.add_item 1
       expect(cart.empty?).to be false
     end
 
     it "如果加 相同種類商品 到購物車，購買項目（CartItem）並不會增加，但 商品數量 會改變" do
-      cart = Cart.new
       3.times { cart.add_item(1) }
       5.times { cart.add_item(2) }
       7.times { cart.add_item(3) }
@@ -20,10 +22,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "商品可以放到購物車裡，也可以再拿出來" do
-      cart = Cart.new
-      p1 = Product.create(title: "海賊王")
-      p2 = Product.create(title: "美食的俘虜")
-
       2.times { cart.add_item(p1.id) }
       3.times { cart.add_item(p2.id) }
 
@@ -33,10 +31,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "每個 Cart Item 都可以計算它自己的金額(小計)" do
-      cart = Cart.new
-      p1 = Product.create(title: "海賊王", price:80)
-      p2 = Product.create(title: "美食的俘虜", price:100)
-
       3.times { cart.add_item(p1.id) }
       5.times { cart.add_item(p2.id) }
 
@@ -45,10 +39,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "可以計算整台購物車的總消費金額" do
-      cart = Cart.new
-      p1 = Product.create(title: "海賊王", price:80)
-      p2 = Product.create(title: "美食的俘虜", price:100)
-
       3.times {
         cart.add_item(p1.id)
         cart.add_item(p2.id)
@@ -58,10 +48,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "特別活動可能可搭配折扣（例如聖誕節時全面打9折，或滿額滿千送百）" do
-      cart = Cart.new
-      p1 = Product.create(title: "海賊王", price:80)
-      p2 = Product.create(title: "美食的俘虜", price:100)
-
       5.times { cart.add_item(p1.id) }
       3.times { cart.add_item(p2.id) }
 
